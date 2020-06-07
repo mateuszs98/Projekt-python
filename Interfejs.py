afb009db414a06fdaced8eb4f73f232d6d86749c
mateuszs98 <65398360+mateuszs98@users.noreply.github.com>
2020-06-07 17:18:30 +0200

    Update Interfejs.py

--- Interfejs.py
+++ Interfejs.py
@@ -1,5 +1,8 @@
 from builtins import print
 import pygame
+import math
+import random
+
 
 class Window:
     def button(self):
@@ -32,3 +35,73 @@ class Field:
 
     def changeColor(self, color):  #ustawianie koloru
         self.color = color
+        
+class Ship:
+
+    def __init__(self, length):
+        self.x = 0
+        self.y = 0
+        self.length = length
+        self.color = (153,102,204)
+        self.setHorizontal = True
+        self.xcoordinate = 0
+        self.ycoordinate = 0
+
+    def setPosition(self,mx,my,screen): #wybieranie pozycji dla statku
+        self.x = mx
+        self.y = my
+        if self.setHorizontal == True:
+            self.drawShipHorizontal(screen)
+        else:
+            self.drawShipVertical(screen)
+
+    def getPositionInField(self):  #ustalanie pozycji na planszy
+        coordinates = []
+        self.xcoordinate = math.floor(self.x / Field.size)
+        self.ycoordinate = math.floor(self.y / Field.size)
+        coordinates.append(self.xcoordinate)
+        coordinates.append(self.ycoordinate)
+
+        if (self.setHorizontal == True):
+            coordinates.append(self.length)
+            coordinates.append(1)
+        else:
+            coordinates.append(1)
+            coordinates.append(self.length)
+        coordinates.append(self.setHorizontal)
+        return coordinates
+
+    def drawShipHorizontal(self,screen):
+        self.setHorizontal = True
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, Field.size*self.length, Field.size))
+
+    def drawShipVertical(self, screen):
+        self.setHorizontal = False
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, Field.size, Field.size* self.length))
+
+class AlienShip(Ship):    #klasa dziedzicząca po Ship
+
+    def __init__(self, length):
+            super().__init__(length)
+            self.x = random.randint(0, 9)
+            self.y = random.randint(0, 9)
+            self.randHorizontal = random.randint(0, 1)
+            if (self.randHorizontal == 0):
+                self.setHorizontal = True
+            else:
+                self.setHorizontal = False
+
+    def getPositionInField(self):  # ustalanie pozycji na planszy
+        coordinates = []
+        coordinates.append(self.x)
+        coordinates.append(self.y)
+
+        if (self.setHorizontal == True):
+            coordinates.append(self.length)
+            coordinates.append(1)
+        else:
+            coordinates.append(1)
+            coordinates.append(self.length)
+        coordinates.append(self.setHorizontal)
+
+        return coordinates

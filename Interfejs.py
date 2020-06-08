30be9e9578dccc6a5a5df8b97d591d0128f42084
mateuszs98 <65398360+mateuszs98@users.noreply.github.com>
2020-06-08 11:32:23 +0200

    Update Interfejs.py

--- Interfejs.py
+++ Interfejs.py
@@ -2,7 +2,7 @@ from builtins import print
 import pygame
 import math
 import random
-
+from MyException import *
 
 class Window:
     def button(self):
@@ -35,7 +35,7 @@ class Field:
 
     def changeColor(self, color):  #ustawianie koloru
         self.color = color
-        
+
 class Ship:
 
     def __init__(self, length):
@@ -105,3 +105,119 @@ class AlienShip(Ship):    #klasa dziedzicząca po Ship
         coordinates.append(self.setHorizontal)
 
         return coordinates
+class Board:  #plansza gry
+    maxSize = 10
+    def __init__(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
+        self.field = []
+        self.statusOfshootedField = False
+        self.counterOfshootFields = 0
+        self.saveColor = (255,255,255)
+        self.rememberPositionOfShip =[]
+        self.field = [[Field(i,j) for j in range(self.maxSize)] for i in range(self.maxSize)] #list comprehension
+
+    checkSizes = lambda mx, my: True if 0 <= mx <= 9 and 0 <= my <= 9 else False   #lambda do sprawdzania rozmiaru statku
+
+    def drawBoard(self, screen):   # rysowanie mapy gry
+        for i in range(self.maxSize):
+            for j in range(self.maxSize):
+                self.field[i][j].drawField(screen, self.dx, self.dy, i, j)#zmiana
+
+    def changeColorField(self,mx,my):   #ustawianie koloru naszego pola po strzale komputera
+
+        positionmx = (mx - self.dx) // Field.size
+        positionmy = (my - self.dy) // Field.size
+
+        if (Board.checkSizes(positionmx,positionmy) == True):
+            if(self.field[positionmx][positionmy].shootAgain == False):
+                if(self.field[positionmx][positionmy].shootAlienShip == True):
+                    self.field[positionmx][positionmy].changeColor((255,0,0))
+                    self.counterOfshootFields += 1
+                    self.saveColor = (255,0,0)
+                    self.field[positionmx][positionmy].shootAgain = True
+                else:
+                    self.field[positionmx][positionmy].changeColor((0, 0, 255))
+                    self.saveColor = (0, 0, 255)
+                    self.field[positionmx][positionmy].shootAgain = True
+            else:
+                return False
+        else:
+            return False
+        return True
+
+    def changeColorFieldbyAlien(self,mx,my):  #ustawianie koloru pól przeciwnika po strzale
+        if (Board.checkSizes(mx, my) == True):
+            if (self.field[mx][my].shootShip == True):
+                self.field[mx][my].changeColor((255, 0, 0))
+                self.counterOfshootFields += 1
+                return True
+            else:
+                self.field[mx][my].changeColor((0, 0, 255))
+        return False
+
+
+    def drawShip(self, myShip):    #rysuje statki na swojej planszy
+        if not isinstance(myShip, Ship):
+            raise MyException("Bledna instancja w funkcji drawShip")
+
+        positionmx = myShip.x // Field.size
+        positionmy = myShip.y // Field.size
+
+        if (self.checkBoundaries(myShip, False) == True):
+            if (myShip.setHorizontal == True):
+                for i in range(0, myShip.length):
+                    self.field[positionmx + i][positionmy].changeColor((153,102,204))
+                    self.field[positionmx + i][positionmy].shootShip= True
+                    self.field[positionmx + i][positionmy].howLongShip = myShip.length #ustawiam dlugosc statku dla informacji czy zatopiony
+            else:
+                for i in range(0, myShip.length):
+                    self.field[positionmx][positionmy + i].changeColor((153,102,204))
+                    self.field[positionmx][positionmy + i].shootShip = True
+                    self.field[positionmx][positionmy + i].howLongShip = myShip.length #ustawiam dlugosc statku dla informacji czy zatopiony
+            return True
+        else:
+            print("Nie możesz tu umieścić statku!")
+            return False
+
+    def checkBoundaries(self,myShip,alien): #sprawdzamy granice statku
+        if not isinstance(myShip, Ship):
+            raise MyException("Bledna instancja w funkcji checkBoundaries")
+        if (alien == False):
+            information = myShip.getPositionInField()
+            x = information[0]
+            y = information [1]
+            width = information [2]
+            height = information [3]
+        else:
+            information = myShip.getPositionInField()
+            x = information[0]
+            y = information[1]
+            width = information[2]
+            height = information[3]
+
+        if x < 0 or x + width > 10 or y < 0 or y + height > 10:
+            return False
+
+        for i in range(x, width + x):
+            for j in range(y, height + y):
+                if self.field[i][j].blocked == True:
+                    return False
+
+        width = width + 1
+        height = height + 1
+        if x - 1 >= 0:
+            if x != self.maxSize - 1 and x + width <= self.maxSize:
+                width += 1
+            x -= 1
+
+        if y - 1 >= 0:
+            if y != self.maxSize - 1 and y + height <= self.maxSize:
+                height += 1
+            y -= 1
+
+        for i in range(x, width + x):
+            for j in range(y, height + y):
+                self.field[i][j].blocked = True
+
+        return True
